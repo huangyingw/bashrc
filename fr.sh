a66ceb6d590698e89241aa6b361bf70c16891a58
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-02 23:15:31 +0800

    n

--- fr.sh
+++ fr.sh
@@ -1,10 +1,6 @@
 #! /bin/sh
-#把chart换成bbs 把pro换成grup 
-#sed -e 's/chart/bbs/;s/pro/grup/' < /media/volgrp/myproject/git/linux/shell/sed/file
-#
-##将匹配行中的--open-files-limit=替换为空, 即删除 
-#echo "--open-files-limit=1234888" | sed -e "s;--open-files-limit=;;" 
-##或 
-#echo "--open-file-limit=1234888"|sed -e "s/--open-file-limit=//" 
+find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -exec grep -wnH  "$2" {} \;
+find "$1" \( -name \.git -o -name \.svn -o -name \.hg -o -path \/media\/smb \) -prune -type f -o -name "$2"
 echo "$1" | sed -e 's/chart/bbs/;s/pro/grup/'
 #echo "x"
+/root/myproject/git/c_c++/googletest/
