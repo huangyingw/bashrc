05d4b99b7cbc89ada763ac1e7e74828542d283cc
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-04 17:15:28 +0800

    n

--- fr.sh
+++ fr.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -exec grep -wnH  "$2" {} \;
-find "$1" \( -name \.git -o -name \.svn -o -name \.hg -o -path \/media\/smb \) -prune -type f -o -name "$2"
-echo "$1" | sed -e 's/chart/bbs/;s/pro/grup/'
-#echo "x"
-/root/myproject/git/c_c++/googletest/
+#find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -exec sed -e 's/chart/bbs/;s/pro/grup/' {} \;
+find $1 \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -exec sed -ie "s/$2/$3/gi" {} \;
+#echo "s/$2/$3/gi"
