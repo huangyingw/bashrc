b29bf5163ae606dcb48697c2a7526b5212da7a1d
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-05 16:22:24 +0800

    n

--- fr.sh
+++ fr.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
-#find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -exec sed -e 's/chart/bbs/;s/pro/grup/' {} \;
-find $1 \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -exec sed -ie "s/$2/$3/gi" {} >>log \;
+find $1 \( -name \.svn -o -path \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -type f | while read -r file
+do
+  sed -n "s/$2/$3/gi;p" $file  >> log
+  sed -ie "s/$2/$3/gi" $file
+done
+
