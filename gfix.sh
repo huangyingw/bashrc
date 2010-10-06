c053f16990ef8a8f361970839a7801398d9abc55
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-06 15:54:16 +0800

    n

--- gfix.sh
+++ gfix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git branch -D fix
-for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push $ss:fix; done
+for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push $ss :fix; done
 git checkout -b fix
