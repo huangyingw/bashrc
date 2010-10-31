75f2db9199ccedd4b28adcc827ee3e921a2d9af0
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-31 04:29:32 -0400

    n

--- fw.sh
+++ fw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$3" ];
 then
-		find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.hpp -o -name \*\.cpp -o -name \*\.sh -o -name \*\.py -o -name YBUILD -o -name [m,M]akefile \) -exec grep -wnH  "$2" {} \;
+  find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.hpp -o -name \*\.cpp -o -name \*\.sh -o -name \*\.py -o -name YBUILD -o -name [m,M]akefile \) -exec grep -nHE "^((|.)$2|(/[^/]|[^/].).*$2).*" {} \;
 else
 		find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h  -o -name \*\.hpp -o -name \*\.cpp -o -name \*\.sh -o -name \*\.py -o -name YBUILD -o -name [m,M]akefile \) -exec  grep -wnHA$3 -B$3  "$2" {} \;
 fi
