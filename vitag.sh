031f66d1dfb750f5aa3c2cbf66ba4fe8b6126ca4
huangyingw@linux_programming <huangyingw@gmail.com>
2011-05-29 22:21:31 +0800

    n

--- vitag.sh
+++ vitag.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 TARGET='/export/home1/username/cscope_db/cscope.files'
-FILE_POSTFIX=$HOME/bashrc/postfix
-echo eval find $(cat ${FILE_POSTFIX}|{ read suf; echo -n "-name '*.$suf'";while read suf;do echo -n " -o -name '*.$suf'";done; })|sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/'
-#eval find $(cat ${FILE_POSTFIX}|{ read suf; echo -n "-name '*.$suf'";while read suf;do echo -n " -o -name '*.$suf'";done; })|sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/' > ${TARGET}
-#cscope -kqR -i ${TARGET} 
+find `pwd`/ -type f -name "*.h" \
+  -o -name "*.py" \
+  -o -name "*.xml" \
+  -o -name "*.properties" \
+  -o -name "*.jsp" \
+  -o -name "*.c" \
+  -o -name "*.cc" \
+  -o -name "*.cpp" \
+  -o -name "*.hpp" \
+  -o -name "*.java" \
+  -o -name "*.js" \
+  |sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/' \
+> ${TARGET}
+cscope -kqR -i ${TARGET} 
