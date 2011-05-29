bc569e3c3519cbacd765fcd0180e2d7f5df78c10
huangyingw@linux_programming <huangyingw@gmail.com>
2011-05-29 22:14:03 +0800

    n

--- vitag.sh
+++ vitag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TARGET='/export/home1/username/cscope_db/cscope.files'
 FILE_POSTFIX=$HOME/bashrc/postfix
-eval find $(cat ${FILE_POSTFIX}|{ read suf; echo -n "-name '*.$suf'";while read suf;do echo -n " -o -name '*.$suf'";done; })|sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/' > ${TARGET}
-cscope -kqR -i ${TARGET} 
+echo eval find $(cat ${FILE_POSTFIX}|{ read suf; echo -n "-name '*.$suf'";while read suf;do echo -n " -o -name '*.$suf'";done; })|sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/'
+#eval find $(cat ${FILE_POSTFIX}|{ read suf; echo -n "-name '*.$suf'";while read suf;do echo -n " -o -name '*.$suf'";done; })|sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/' > ${TARGET}
+#cscope -kqR -i ${TARGET} 
