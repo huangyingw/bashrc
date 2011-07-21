ab2bf06cb87f99c3b6981314868ee55409cc98cb
huangyingw@linux_programming <huangyingw@gmail.com>
2011-07-21 15:57:27 +0800

    n

--- fw.sh
+++ fw.sh
@@ -21,7 +21,7 @@ then
  -exec fgrep -wnH  "$2" {} \;
 else
 		find "$1" \( -name \.svn -o -name \.git -o -name \.hg -o -name find.cc \) -prune -o \(  \
-      -o -name \*\.cc \
+      -name \*\.cc \
       -o -name \*\.java \
       -o -name \*\.xml \
       -o -name \*\.cpp \
@@ -35,5 +35,6 @@ else
       -o -name YBUILD \
       -o -name \.gitignore \
       -o -name [mM]akefile \
+ \) \
  -exec  fgrep -wnHA$3 -B$3  "$2" {} \;
 fi
