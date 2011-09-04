a78c8bc9b96c4c322833254804e1a243f1dc6c85
huangyingw@linux_programming <huangyingw@gmail.com>
2011-09-04 17:11:09 +0800

    n

--- type_rsync.sh
+++ type_rsync.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+SOURCE=$1
+TARGET=$2
 FILE_TYPE=$HOME/bashrc/file_type
 command_params=`cat ${FILE_TYPE}|{ read suf; echo -n "--include=*.$suf";while read suf;do echo -n " --include=*.$suf";done; }`
-rsync -avH --include=*/  $command_params --exclude=* --prune-empty-dirs  /media/comprehensive/storage/learning/English/ /media/usb/storage/learning/English/
+
+if [ "$SOURCE" != "$TARGET" ]
+then
+  if [ ! -d "$TARGET" ];
+  then   
+    mkdir -p "$TARGET"
+  fi
+  rsync -avH --include=*/  $command_params --exclude=* --prune-empty-dirs "$SOURCE" "$TARGET"
+else
+  echo "please choose the different dir!"
+fi
