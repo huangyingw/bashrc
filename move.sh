0ac87eb22ec3d657b81ab701a76cf96bbfe9305b
huangyingw@linux_programming <huangyingw@gmail.com>
2012-04-07 14:36:39 -0500

    n

--- move.sh
+++ move.sh
@@ -16,21 +16,19 @@ IsSlash "$1"
 SOURCE=$returnstring
 IsSlash "$2"
 TARGET=$returnstring
-echo $SOURCE
-echo $TARGET
 
-#if [ "$SOURCE" != "$TARGET" ]
-#then
-#  if [ ! -d "$TARGET" ];
-#  then   
-#    mkdir -p "$TARGET"
-#  fi
-#
-#  rsync -aH \
-  #    "$SOURCE" "$TARGET" \
-  #    && rsync -aH \
-  #    "$SOURCE" "$TARGET" \
-  #    && rm -fr "$SOURCE"
-#else
-#  echo "please choose the different dir!"
-#fi
+if [ "$SOURCE" != "$TARGET" ]
+then
+  if [ ! -d "$TARGET" ];
+  then   
+    mkdir -p "$TARGET"
+  fi
+
+  rsync -aH \
+    "$SOURCE" "$TARGET" \
+    && rsync -aH \
+    "$SOURCE" "$TARGET" \
+    && rm -fr "$SOURCE"
+else
+  echo "please choose the different dir!"
+fi
