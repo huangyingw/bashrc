d318c4d8c9a7b6d99ec23d219d2dda1c788fac03
huangyingw@linux_programming <huangyingw@gmail.com>
2012-04-07 14:15:54 -0500

    n

--- move.sh
+++ move.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-SOURCE=$1
+case "$1" in
+  */)
+    SOURCE=$1
+    ;;
+  *)
+    SOURCE=$1"/"
+    ;;
+esac
+echo $SOURCE
 TARGET=$2
 
-if [ "$SOURCE" != "$TARGET" ]
-then
-  if [ ! -d "$TARGET" ];
-  then   
-    mkdir -p "$TARGET"
-  fi
-
-  rsync -aH \
-    "$SOURCE" "$TARGET" \
-    && rsync -aH \
-    "$SOURCE" "$TARGET" \
-    && rm -fr "$SOURCE"
-else
-  echo "please choose the different dir!"
-fi
+#if [ "$SOURCE" != "$TARGET" ]
+#then
+#  if [ ! -d "$TARGET" ];
+#  then   
+#    mkdir -p "$TARGET"
+#  fi
+#
+#  rsync -aH \
+  #    "$SOURCE" "$TARGET" \
+  #    && rsync -aH \
+  #    "$SOURCE" "$TARGET" \
+  #    && rm -fr "$SOURCE"
+#else
+#  echo "please choose the different dir!"
+#fi
