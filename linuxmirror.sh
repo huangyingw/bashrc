14c761d73d9e7ab28e952c5e179a04732fa4f0af
huangyingw@linux_programming <huangyingw@gmail.com>
2012-05-16 08:48:02 -0500

    n

--- linuxmirror.sh
+++ linuxmirror.sh
@@ -10,9 +10,7 @@ fi
 rsync -aH --delete-during \
   --exclude fstab \
   --exclude grub.cfg \
-  --exclude hosts \
   --exclude mdadm.conf \
-  --exclude hostname \
   --exclude /boot/initrd.img-* \
   --exclude /boot/vmlinuz-* \
   --exclude /var/lib/mldonkey/temp/ \
