bb091372f5c5e43e88aa9fd2b64fe84b70483b6d
huangyingw@linux_programming <huangyingw@gmail.com>
2012-08-07 22:33:46 -0500

    n

--- fgh.sh
+++ fgh.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-ls .|while read ss
+function iterate_dir(){
+for file in "$1"/*
 do
-  if [ -d $ss ];
+  if [ -d "$file" ];
   then
-    cd $ss
-    if  [ -d ".git" ];
-    then   
+    if [ -d "$file/.git" ];
+    then
+      cd "$file"
       $HOME/bashrc/gagh.sh
+    else
+      iterate_dir "$file"
     fi
-    cd ..
   fi
 done
+}
+if [ -z "$1" ];
+then
+  iterate_dir .
+else
+  iterate_dir "$1"
+fi
