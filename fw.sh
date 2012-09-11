3cf75e8d736188a3ba1765a5b127c63728d3b74b
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-11 10:58:14 -0500

    n

--- fw.sh
+++ fw.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 if [ -z "$3" ];
 then
-		find "$1" \( -name \.svn -o -name \.git -o -name \.hg -o -name find.cc \) -prune -o \( \
-      -iname \*\.c \
-      -o -iname \*\.cc \
-      -o -iname \*\.java \
-      -o -iname \*\.xml \
-      -o -iname \*\.cpp \
-      -o -iname \*\.hpp \
-      -o -iname \*\.js \
-      -o -iname \*\.jsp \
-      -o -iname \*\.properties \
-      -o -iname \*\.h \
-      -o -iname \*\.sh \
-      -o -iname \*\.py \
-      -o -iname \*\.tag \
-      -o -iname \*\.sql \
-      -o -iname \*\.xml.model \
-      -o -iname YBUILD \
-      -o -iname \.gitignore \
-      -o -iname [mM]akefile \
-      -o -iname \*\.php \
-      -o -iname \*\.js \
-      -o -iname \*\.css \
-      -o -iname \*\.htm \
-      -o -iname \*\.html \
- \) \
- -exec fgrep -wnH  "$2" {} \;
+  find "$1" \( -name \.svn -o -name \.git -o -name \.hg -o -name find.cc \) -prune -o \( \
+    -iname \*\.c \
+    -o -iname \*\.cc \
+    -o -iname \*\.java \
+    -o -iname \*\.xml \
+    -o -iname \*\.cpp \
+    -o -iname \*\.hpp \
+    -o -iname \*\.js \
+    -o -iname \*\.jsp \
+    -o -iname \*\.properties \
+    -o -iname \*\.h \
+    -o -iname \*\.sh \
+    -o -iname \*\.py \
+    -o -iname \*\.tag \
+    -o -iname \*\.sql \
+    -o -iname \*\.xml.model \
+    -o -iname YBUILD \
+    -o -iname \.gitignore \
+    -o -iname [mM]akefile \
+    -o -iname \*\.php \
+    -o -iname \*\.js \
+    -o -iname \*\.css \
+    -o -iname \*\.htm \
+    -o -iname \*\.html \
+    \) \
+    -exec fgrep -wnH  "$2" {} \;
 else
-		find "$1" \( -name \.svn -o -name \.git -o -name \.hg -o -name find.cc \) -prune -o \(  \
-      -iname \*\.cc \
-      -o -iname \*\.java \
-      -o -iname \*\.xml \
-      -o -iname \*\.cpp \
-      -o -iname \*\.hpp \
-      -o -iname \*\.js \
-      -o -iname \*\.jsp \
-      -o -iname \*\.properties \
-      -o -iname \*\.h \
-      -o -iname \*\.sh \
-      -o -iname \*\.py \
-      -o -iname \*\.tag \
-      -o -iname \*\.sql \
-      -o -iname \*\.xml.model \
-      -o -iname YBUILD \
-      -o -iname \.gitignore \
-      -o -iname [mM]akefile \
-      -o -iname \*\.php \
-      -o -iname \*\.js \
-      -o -iname \*\.css \
-      -o -iname \*\.htm \
-      -o -iname \*\.html \
- \) \
- -exec  fgrep -wnHA$3 -B$3  "$2" {} \;
+  find "$1" \( -name \.svn -o -name \.git -o -name \.hg -o -name find.cc \) -prune -o \(  \
+    -iname \*\.cc \
+    -o -iname \*\.java \
+    -o -iname \*\.xml \
+    -o -iname \*\.cpp \
+    -o -iname \*\.hpp \
+    -o -iname \*\.js \
+    -o -iname \*\.jsp \
+    -o -iname \*\.properties \
+    -o -iname \*\.h \
+    -o -iname \*\.sh \
+    -o -iname \*\.py \
+    -o -iname \*\.tag \
+    -o -iname \*\.sql \
+    -o -iname \*\.xml.model \
+    -o -iname YBUILD \
+    -o -iname \.gitignore \
+    -o -iname [mM]akefile \
+    -o -iname \*\.php \
+    -o -iname \*\.js \
+    -o -iname \*\.css \
+    -o -iname \*\.htm \
+    -o -iname \*\.html \
+    \) \
+    -exec  fgrep -wnHA$3 -B$3  "$2" {} \;
 fi
