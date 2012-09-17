04d2c8f1406e7217865de73ba8e3c03100cb2ac4
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-16 22:39:43 -0500

    n

--- vitag.sh
+++ vitag.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 TARGET='/export/home1/username/cscope_db/cscope.files'
 FILE_POSTFIX=$HOME/bashrc/postfix
-command_params=`cat ${FILE_POSTFIX}|{ read suf; echo -n "-name '*.$suf'";while read suf;do echo -n " -o -name '*.$suf'";done; }`
-eval find `pwd`/ $command_params -o -iname makefile |sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/' > ${TARGET}
+find_params=(); or="";
+while read suf
+do
+  find_params+=( $or "-name" "*.$suf" )
+  or="-o"
+done < "$FILE_POSTFIX"
+find "$PWD" "${find_params[@]}" -o -iname makefile |sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > ${TARGET}
 cscope -kqR -i ${TARGET} 
