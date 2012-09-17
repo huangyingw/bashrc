f0f53f815438aa49ed1c9519c88d597a4edabadc
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-17 08:31:55 -0500

    n

--- fw.sh
+++ fw.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 FILE_POSTFIX=$HOME/bashrc/postfix
-find_params=(); or="";
+find_params=();
+or="";
+grep_params="";
+if [ -n "$3" ]
+then grep_params=" -A"$3" -B"$3;
+fi
 while read suf
 do
   find_params+=( $or "-iname" "*.$suf" )
   or="-o"
 done < "$FILE_POSTFIX"
-find "$1" "(" "${find_params[@]}" ")" -exec fgrep -wnH  "$2" {} \;
+find "$1" "(" "${find_params[@]}" ")" -exec fgrep -wnH  $grep_params "$2" {} \;
