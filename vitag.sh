7b747b7659b26d2822d528e3d3965f05ba944641
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-17 10:10:58 -0500

    n

--- vitag.sh
+++ vitag.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 TARGET='/export/home1/username/cscope_db/cscope.files'
 FILE_POSTFIX=$HOME/bashrc/postfix
-find_params=(); or="";
+PRUNE_POSTFIX=$HOME/bashrc/prunefix
+find_params=();
+or="";
+prune_params=();
 while read suf
 do
   find_params+=( $or "-name" "*.$suf" )
   or="-o"
 done < "$FILE_POSTFIX"
-find "$PWD" "${find_params[@]}" -o -iname makefile |sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > ${TARGET}
+or="";
+while read suf
+do
+  prune_params+=( $or "-iname" "*.$suf" )
+  or="-o"
+done < "$PRUNE_POSTFIX"
+find "$PWD" "${prune_params[@]}" -prune -o "${find_params[@]}" -o -iname makefile |sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > ${TARGET}
 cscope -kqR -i ${TARGET} 
