759b76556c55df14117acdf23ab5831a6fb73abd
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-20 17:39:03 -0500

    n

--- fw.sh
+++ fw.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 FILE_POSTFIX=$HOME/bashrc/postfix
 PRUNE_POSTFIX=$HOME/bashrc/prunefix
+PRUNE_FILE=$HOME/bashrc/prunefile
 find_params=();
 prune_params=();
+prune_files=();
 or="";
 grep_params="";
 if [ -n "$3" ]
@@ -19,4 +21,9 @@ do
   prune_params+=( $or "-iname" "*.$suf" )
   or="-o"
 done < "$PRUNE_POSTFIX"
-find "$1" "(" "${prune_params[@]}" "-o" "-iname" "find.cc" ")" -prune -o "(" "${find_params[@]}" "-o" "-iname" "makefile" ")" -exec fgrep -wnH  $grep_params "$2" {} \;
+while read suf
+do
+  prune_files+=( $or "-iname" "$suf" )
+  or="-o"
+done < "$PRUNE_FILE"
+find "$1" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -prune -o "(" "${find_params[@]}" "-o" "-iname" "makefile" ")" -exec fgrep -wnH  $grep_params "$2" {} \;
