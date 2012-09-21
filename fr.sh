75e36efb9b58f44ff1d601b3b557b335d31bb255
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-21 14:43:47 -0500

    n

--- fr.sh
+++ fr.sh
@@ -1,7 +1,33 @@
 #! /bin/sh
-find $1 \( -path \.svn -o -path \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc  -o -name \*\.properties -o -name \.gitignore -o -name \*\.h -o -name \*\.cpp -o -name \*\.hpp -o -name \*\.sh -o -name YBUILD -o -name [m,M]akefile \) -type f | while read -r file
+FILE_POSTFIX=$HOME/bashrc/postfix
+PRUNE_POSTFIX=$HOME/bashrc/prunefix
+PRUNE_FILE=$HOME/bashrc/prunefile
+find_params=();
+prune_params=();
+prune_files=();
+or="";
+grep_params="";
+if [ -n "$3" ]
+then grep_params=" -A"$3" -B"$3;
+fi
+while read suf
 do
-  sed -n "s|\<$2\>|\<$3\>|gi;p" $file  >> log
-  sed -i "s|\<$2\>|\<$3\>|gi" $file
+  find_params+=( $or "-iname" "*.$suf" )
+  or="-o"
+done < "$FILE_POSTFIX"
+or="";
+while read suf
+do
+  prune_params+=( $or "-iname" "*.$suf" )
+  or="-o"
+done < "$PRUNE_POSTFIX"
+while read suf
+do
+  prune_files+=( $or "-iname" "$suf" )
+  or="-o"
+done < "$PRUNE_FILE"
+find "$1" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -prune -o "(" "${find_params[@]}" "-o" "-iname" "makefile" ")" -type f | while read -r file
+do
+  sed -i "s/"$2"/"$3"/gi" $file
 done
 
