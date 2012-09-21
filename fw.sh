a92bf74b20f78846eed7e5a68762d2a7a122674a
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-21 14:26:37 -0500

    n

--- fw.sh
+++ fw.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+build_parameter() {
+  toReturns=();
+  or=$1;
+  while read suf
+  do
+    toReturns+=( $or "-iname" $2$suf )
+    or="-o"
+  done < "$3"
+  return $toReturns
+}
 FILE_POSTFIX=$HOME/bashrc/postfix
 PRUNE_POSTFIX=$HOME/bashrc/prunefix
 PRUNE_FILE=$HOME/bashrc/prunefile
@@ -10,20 +20,10 @@ grep_params="";
 if [ -n "$3" ]
 then grep_params=" -A"$3" -B"$3;
 fi
-while read suf
-do
-  find_params+=( $or "-iname" "*.$suf" )
-  or="-o"
-done < "$FILE_POSTFIX"
-or="";
-while read suf
-do
-  prune_params+=( $or "-iname" "*.$suf" )
-  or="-o"
-done < "$PRUNE_POSTFIX"
-while read suf
-do
-  prune_files+=( $or "-iname" "$suf" )
-  or="-o"
-done < "$PRUNE_FILE"
-find "$1" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -prune -o "(" "${find_params[@]}" "-o" "-iname" "makefile" ")" -exec fgrep -wnH  $grep_params "$2" {} \;
+build_parameter "" "*." $FILE_POSTFIX
+find_params=$?
+build_parameter "" "*." $PRUNE_POSTFIX
+prune_params=$?
+build_parameter "-o" "" $PRUNE_FILE
+prune_files=$?
+echo find "$1" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -prune -o "(" "${find_params[@]}" "-o" "-iname" "makefile" ")" -exec fgrep -wnH  $grep_params "$2" {} \;
