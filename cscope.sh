4eb4715aecd5175d5ea55fb93fe98bdca7c0ce70
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-26 18:27:46 -0500

    n

---
+++ cscope.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TARGET='/export/home1/username/cscope_db/cscope.files'
+FILE_POSTFIX=$HOME/bashrc/postfix
+PRUNE_POSTFIX=$HOME/bashrc/prunefix
+find_params=();
+or="";
+prune_params=();
+while read suf
+do
+  find_params+=( $or "-name" "*.$suf" )
+  or="-o"
+done < "$FILE_POSTFIX"
+or="";
+while read suf
+do
+  prune_params+=( $or "-iname" "*.$suf" )
+  or="-o"
+done < "$PRUNE_POSTFIX"
+find "$PWD" "(" "${prune_params[@]}" "-o" "-iname" "find.cc" ")" -a -prune -o "(" "${find_params[@]}" "-o" "-iname" "makefile" ")" -print |sed 's/\(["'\''\]\)/\\\1/g;s/.*/"&"/' > ${TARGET}
+cscope -bkqR -i ${TARGET} 
