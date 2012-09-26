e4f8e387ca6631ebbfae6ebbf20d883ed9953523
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-26 18:37:57 -0500

    n

--- fgs.sh
+++ fgs.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-ls .|while read ss
+folders=();
+find . -type d|while read ss
 do
-  if [ -d $ss ];
-  then
-    cd $ss
-    if  [ -d ".git" ];
-    then   
-      if  ( git status|grep -q modified: )
-      then
-        echo `pwd` need to save
-      fi
-    else
-      echo `pwd` need to init
-    fi
-    cd ..
-  fi
+  folders+="$ss"
 done
+echo ${folders[@]}
+for foldera in ${folders[@]} 
+do
+  echo $foldera 
+done
+#ls .|while read ss
+#do
+#  if [ -d $ss ];
+#  then
+#    cd $ss
+#    if  [ -d ".git" ];
+#    then   
+#      if  ( git status|grep -q modified: )
+#      then
+#        echo `pwd` need to save
+#      fi
+#    else
+#      echo `pwd` need to init
+#    fi
+#    cd ..
+#  fi
+#done
