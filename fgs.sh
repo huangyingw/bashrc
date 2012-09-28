2adbdce9ab84b9e655f6d417e5b21b84a0614e42
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-28 11:12:38 -0500

    n

--- fgs.sh
+++ fgs.sh
@@ -2,17 +2,20 @@
 function rec_dir() {
 for file in `ls $1`
 do
-  if [ -d $1"/"$file -a ! -d $1"/"$file"/.git" ]
+  if [ -d $1"/"$file ]
   then
-    rec_dir $1"/"$file
-  else
-    cd $1"/"$file
-    if  ( git status|grep -q modified: )
+    if [ ! -d $1"/"$file"/.git" ]
     then
-      echo `pwd` need to save
-    fi 
-    cd ..
-  fi
+      rec_dir $1"/"$file
+    else
+      cd $1"/"$file
+      if  ( git status|grep -q modified: )
+      then
+        echo `pwd` need to checkin
+      fi 
+      cd $1
+    fi
+  fi 
 done
 }
 rec_dir .
