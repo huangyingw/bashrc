ccbe1af8b2839bbc5b73635f74f4e05beaef2a8e
huangyingw@linux_programming <huangyingw@gmail.com>
2012-09-29 18:07:43 -0500

    n

--- fr.sh
+++ fr.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 file=/home/huangyingw/myproject/git/test_fr/makefile
-sed -i "s/"$1"/"$2"/gi" $file
+#sed -i "s/"$1"/"$2"/gi" $file
+#sed -i 's/"$1"/"$2"/gi' $file
+#sed -i "s#\"$1\"#\"$2\"#gi" $file
+sed "s#$1#$2#g" "$file"
