6b17e8110746e366d76843931b86d8caa95ae173
huangyingw@linux_programming <huangyingw@gmail.com>
2012-10-01 23:02:50 -0500

    n

--- tubak.sh
+++ tubak.sh
@@ -4,11 +4,15 @@ UBAKIN=$HOME/bashrc/ubak_in
 exclude_params=();
 while read suf
 do
-  exclude_params+=( " --exclude=$suf" )
+  exclude_params+=( "--exclude=$suf" )
 done < "$UBAKEX"
 include_params=();
 while read suf
 do
-  include_params+=( " --include=$suf" )
+  include_params+=( "--include=$suf" )
 done < "$UBAKIN"
-echo rsync -aHinv --delete-during "${exclude_params[@]}" "${include_params[@]}" / /media/volgrp/media/laptop/
+i=0
+for x in "${exclude_params[@]}"; do echo $((i++)) "'$x'"; done
+i=0
+for x in "${include_params[@]}"; do echo $((i++)) "'$x'"; done
+rsync -aHinv --delete-during "${exclude_params[@]}" "${include_params[@]}" / /media/volgrp/media/laptop/|vi -
