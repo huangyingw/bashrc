3f264c14483d80a4f6f5d91484fa222a1dd868fd
huangyingw@linux_programming <huangyingw@gmail.com>
2012-10-06 18:35:15 -0500

    n

---
+++ tures.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+SOURCE=$1
+TARGET=$2
+
+if [ ! -d "${TARGET}" ];
+then   
+  mkdir -p "${TARGET}"
+fi
+
+rsync -aH --delete-during \
+  --exclude fstab \
+  --exclude grub.cfg \
+  --exclude mdadm.conf \
+  --exclude /boot/initrd.img-* \
+  --exclude /boot/vmlinuz-* \
+  --exclude /var/lib/mldonkey/temp/ \
+  --exclude 70-persistent-net.rules \
+  --exclude interfaces \
+  --exclude /etc/initramfs-tools/conf.d/resume \
+  "${SOURCE}" "${TARGET}"
