409bd9b3b82ce5d8383e05158cf9d713454e4d8c
huangyingw@linux_programming <huangyingw@gmail.com>
2012-10-14 22:20:26 -0500

    n

--- tures.sh
+++ tures.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
-URESEX=$HOME/bashrc/ures_ex
-URESIN=$HOME/bashrc/ures_in
+if [ -z "$1" ];
+then
+  echo please enter the source path
+  exit
+fi
+if [ -z "$2" ];
+then
+  echo please enter the target path
+  exit
+fi
+URESEX=./ures_ex
+URESIN=./ures_in
 exclude_params=();
 while read suf
 do
@@ -12,8 +22,4 @@ do
   include_params+=( "--include=$suf" )
 done < "$URESIN"
 
-if [ ! -d "${TARGET}" ];
-then   
-  mkdir -p "${TARGET}"
-fi
-rsync -aHinv --delete-during "${exclude_params[@]}" /media/volgrp/media/laptop/ / > result.cc
+rsync -aHinv --delete-during "${exclude_params[@]}" "$1" "$2" > result.cc
