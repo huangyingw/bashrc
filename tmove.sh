6b780ef92f81c4db9b5433a9941da6237e118e7c
huangyingw@linux_programming <huangyingw@gmail.com>
2012-12-23 20:22:52 -0800

    n

--- tmove.sh
+++ tmove.sh
@@ -1,11 +1,31 @@
-#!/bin/bash 
-SOURCE=$1
-TARGET=$2
+#!/bin/bash
+returnstring=""
+function IsSlash()
+{
+  var=$1
+  case $var in
+    */)
+      returnstring=$var 
+      ;;
+    *)
+      returnstring=$var"/"
+      ;;
+  esac
+}
+IsSlash "$1"
+SOURCE=$returnstring
+IsSlash "$2"
+TARGET=$returnstring
 
-if [ ! -d "${TARGET}" ];
-then   
-    mkdir -p "${TARGET}"
+if [ "$SOURCE" != "$TARGET" ]
+then
+  if [ ! -d "$TARGET" ];
+  then   
+    mkdir -p "$TARGET"
+  fi
+
+  rsync -aHinv \
+    "${SOURCE}" "${TARGET}"> find.cc
+else
+  echo "please choose the different dir!"
 fi
-
-rsync -aHinv \
-		"${SOURCE}" "${TARGET}"> find.cc
