7032512a656ec67cd58a58385bf596a18742afeb
huangyingw@linux_programming <huangyingw@gmail.com>
2013-04-11 13:44:44 -0700

    n

--- ff.sh
+++ ff.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 PRUNE_POSTFIX=$HOME/bashrc/prunefix
 prune_params=();
+find_result="$2".findresult
+if [ -f "$find_result" ]; then
+  echo the search is already done, if you want to update, please delete the findresult file first
+  exit 1
+fi
 or="";
 while read suf
 do
   prune_params+=( $or "-iname" "*.$suf" )
   or="-o"
 done < "$PRUNE_POSTFIX"
-find "$1" "(" "${prune_params[@]}" ")" -prune -type f -o -iname "$2"
+find "$1" "(" "${prune_params[@]}" ")" -prune -type f -o -iname "$2" > "$find_result"
