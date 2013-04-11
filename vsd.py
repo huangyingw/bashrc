bf42735eefccf69d3e4502931563be555feb66f5
huangyingw@linux_programming <huangyingw@gmail.com>
2013-04-10 21:48:26 -0700

    n

--- vsd.py
+++ vsd.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python
-
+import xml.etree.cElementTree as ET
+tree = ET.ElementTree(file='hadoop.xml')
+print tree .find('name').text
--- wol.sh
+++ wol.sh
@@ -2,6 +2,7 @@
 if [ -z "$1" ];
 then
   wakeonlan 00:0a:cd:1e:e9:be
+  wakeonlan b8:70:f4:ce:3d:f3
 else
   wakeonlan 00:0a:cd:1e:e9:be
   wakeonlan b8:70:f4:ce:3d:f3
