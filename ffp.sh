9f926e1e0b771441419ed663826fab30b0844450
huangyingw@linux_programming <huangyingw@gmail.com>
2013-04-24 16:26:58 -0700

    after this change, the annoying jquery*.js files disappear from the ff.sh result. And the WEB-INF is deleted from the prunefile

--- ffp.sh
+++ ffp.sh
@@ -15,7 +15,7 @@ do
 done < "$PRUNE_POSTFIX"
 while read suf
 do
-  prune_files+=( $or "-iname" "$suf" )
+  prune_files+=( $or "-iname" "*.$suf" )
   or="-o"
 done < "$PRUNE_FILE"
 find "$1" "(" "${prune_params[@]}" "${prune_files[@]}" ")" -prune -type f -o -ipath *"$2" > "$find_result"
