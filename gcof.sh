04908e4ce7a7ab1f036c8634b6a8579c335b9819
huangyingw@linux_programming <huangyingw@gmail.com>
2013-06-07 17:39:39 -0700

    update the gcof to reset to head, instead of the latest commit of all,and move xsql to mysqlrc folder

--- gcof.sh
+++ gcof.sh
@@ -2,4 +2,4 @@
 # this is used to reset back to the latest change on the file.
 commit=`git log --all -- "$1" |awk 'NR==1{print $2}'`
 cp -fv "$1" "$1".bak \
-  && git checkout "$commit" -- "$1"
+  && git checkout HEAD -- "$1"
--- xsql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ];
-then
-  echo Please provide the specified sql file
-else
-  user="root"
-  pass="1qaz@WSX"
-  mysql -u "$user" -p"$pass" < "$1"
-fi
