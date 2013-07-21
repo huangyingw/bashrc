7694da46b7c816324962fa0979072953baf8d024
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-21 12:38:15 -0700

    update on the wol.sh file

--- wol.sh
+++ wol.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -z "$1" ];
 then
-  wakeonlan 00:0a:cd:1e:e9:be
+  wakeonlan 00:0a:cd:1e:e9:be #server
 else
-  wakeonlan 00:0a:cd:1e:e9:be
   wakeonlan b8:70:f4:ce:3d:f3
   wakeonlan 00:1a:4b:8d:a2:6e
+  wakeonlan 00:01:c0:12:94:60 #mini
 fi
