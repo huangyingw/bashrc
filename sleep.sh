805a93030f2b95a9cf08f30a2d7189f85420a535
huangyingw@linux_programming <huangyingw@gmail.com>
2013-08-06 22:55:43 -0700

    update on sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-dbus-send --system --print-reply     --dest="org.freedesktop.UPower"     /org/freedesktop/UPower     org.freedesktop.UPower.Suspend
+if [ -f /var/run/dbus/system_bus_socket ]; then
+  dbus-send --system --print-reply     --dest="org.freedesktop.UPower"     /org/freedesktop/UPower     org.freedesktop.UPower.Suspend
+else
+  pm-suspend
+fi
+
