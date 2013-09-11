9fb81fe712c8ce78b3ca12204260ed8138ba5934
huangyingw@linux_programming <huangyingw@gmail.com>
2013-09-10 19:48:08 -0700

    update umletc.sh, to massively generate output pdf files

--- umletc.sh
+++ umletc.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-find . -type f -name \*.uxf -exec umlet -action=convert -format=pdf -filename={} \;
+if [ -z "$1" ];
+then
+    echo "Please provide the folder parameter"
+    exit 1
+fi
+find "$1" -type f -name \*.uxf|while read ss
+do 
+    output="`echo "$ss"|sed  -e "s/\.uxf$/\.pdf/g"`"
+    umlet -action=convert -format=pdf -filename="$ss" -output="$output"
+done 
