97f2cdd575a8e624a59d3f4f4735f50b27bececd
huangyingw@linux_programming <huangyingw@gmail.com>
2013-09-14 18:02:29 -0700

    update fav.sh

--- fav.sh
+++ fav.sh
@@ -2,9 +2,9 @@
 if [ -f $HOME/.passwd ]; then
   FAV=`cat $HOME/.passwd |grep fav|awk '{print $3}'`
 fi
-rm /media/volgrp/latest/*
+rm /media/volgrp/fav/*
 find "$FAV" -type f -mtime -100 -size +700M -exec ls -rt {} \+|tail -n 100|while read ss
 do 
     ftemp=`basename "$ss"`
-    ln -s "$ss" /media/volgrp/latest/"$ftemp"
+    ln -s "$ss" /media/volgrp/fav/"$ftemp"
 done
