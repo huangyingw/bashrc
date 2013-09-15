4947c847a6566b23ccda517a314d33e04e828ee4
huangyingw@linux_programming <huangyingw@gmail.com>
2013-09-14 18:00:59 -0700

    set the default parameter for umletc

--- umletc.sh
+++ umletc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if [ -z "$1" ];
 then
-    echo "Please provide the folder parameter"
-    exit 1
+    target=.
+else
+    target="$1"
 fi
-find "$1" -type f -name \*.uxf|while read ss
+find "$target" -type f -name \*.uxf|while read ss
 do 
     output="`echo "$ss"|sed  -e "s/\.uxf$/\.pdf/g"`"
     umlet -action=convert -format=pdf -filename="$ss" -output="$output"
