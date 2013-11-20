402f98f133c962fd57ca6c29d38ac173785de172
huangyingw@linux_programming <huangyingw@gmail.com>
2013-11-19 22:48:33 -0800

    n

--- rsleep.sh
+++ rsleep.sh
@@ -4,7 +4,7 @@ green='\033[0;32m'
 NC='\033[0;0m' # No Color
 if [ -z "$1" ];
 then
-  echo -e "${red}Please provide remote machine name ${NC}"
+  ssh ubuntu pm-suspend
   exit 1
 fi
 ssh "$1" pm-suspend
