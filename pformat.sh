e3179b5f47ccdaca625ec80e7da3204f960a141a
huangyingw@linux_programming <huangyingw@gmail.com>
2014-01-03 13:12:40 -0800

    before initiating new branch format.fix from format,before initiating new branch format from master,n

---
+++ pformat.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
+if [ -n "$1" ];
+then
+  folderForGit="$1"
+else
+  folderForGit=.
+fi
+if  ( git status "$folderForGit"|grep -q 'nothing to commit' )
+then
+  find "$folderForGit" -type f -name \*.py -exec autopep8 --in-place {} \; 
+else
+  echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
+fi 
