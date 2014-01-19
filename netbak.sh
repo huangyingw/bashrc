968c1d0131b75c1be244cc075abe088fff7aec7d
huangyingw@linux_programming <huangyingw@gmail.com>
2014-01-19 15:21:35 -0800

    n

--- netbak.sh
+++ netbak.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-$HOME/bashrc/nubak.sh &&$HOME/bashrc/copy.sh /media/volgrp/ ubuntu:/media/volgrp/&&$HOME/bashrc/rsleep.sh
+$HOME/bashrc/nubak.sh \
+  ; $HOME/bashrc/copy.sh /media/volgrp/ ubuntu:/media/volgrp/ \
+  ; $HOME/bashrc/rsleep.sh
