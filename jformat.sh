07af52e1b97a9b4fbf85ed3faddb60b43a7d91a7
huangyingw@linux_programming <huangyingw@gmail.com>
2014-02-14 14:38:57 -0800

    n

--- jformat.sh
+++ jformat.sh
@@ -10,7 +10,7 @@ else
 fi
 if  ( git status "$folderForGit"|grep -q 'nothing to commit' )
 then
-  find "$folderForGit" -type f -name \*.java -exec astyle -t2 -f --style=java {} \; 
+  find "$folderForGit" -type f -name \*.java -exec astyle --mode=java -fxejoO  -t2 --style=java {} \; 
 else
   echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
 fi
