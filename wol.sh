f1331d50b17177f61d504368cc54882152d5e215
huangyingw@linux_programming <huangyingw@gmail.com>
2014-02-21 19:32:05 -0800

    n

--- wol.sh
+++ wol.sh
@@ -2,8 +2,10 @@
 if [ -z "$1" ]; then
   wakeonlan 00:0a:cd:1e:e9:be #server
   ping ubuntu
-elif [ "$1" == "laptop" ]; then
-  wakeonlan 00:21:cc:c0:53:a9 #laptop
-  wakeonlan b8:70:f4:ce:3d:f3 #laptop
-  ping laptop
+elif [ "$1" == "toshiba" ]; then
+  wakeonlan b8:70:f4:ce:3d:f3 #toshiba
+  ping toshiba
+elif [ "$1" == "thinkpad" ]; then
+  wakeonlan 00:21:cc:c0:53:a9 #thinkpad
+  ping thinkpad
 fi
