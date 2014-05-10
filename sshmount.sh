b11a3580b35023dd6e0f805631dbc6c46a5c9820
huangyingw@linux_programming <huangyingw@gmail.com>
2014-05-10 00:56:42 -0700

    n

--- sshmount.sh
+++ sshmount.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sshfs -o idmap=user `whoami`@ubuntu:/media/ /media/samba/
-if [ $? -ne 0 ]; then
-    sshfs -o idmap=user `whoami`@vdonkey:/media/ /media/samba/
-fi
+fusermount -u /media/samba/ \
+  ; sshfs ubuntu:/media/ /media/samba/ \
+  && df -Th
