b564bfa9bd25416c00f561f4eb25fbedf73c784e
huangyingw@linux_programming <huangyingw@gmail.com>
2014-05-11 23:42:15 -0700

    n

---
+++ ygrep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+find_result="`echo "$1".grep.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
+if [ -f "$find_result" ]; then
+  read -p "the search is already done, if you want to update, press u --> " update
+  case $update in
+    u)
+      ;;
+    ?)
+      vi "$find_result"
+      exit 1
+      ;;
+  esac
+fi
+grep "$2" "$1" -rnH > "$find_result"
+vi "$find_result"
