4679607ff79f62b561db5d3fbd623c57fb0bf261
huangyingw@linux_programming <huangyingw@gmail.com>
2014-05-21 22:36:41 -0700

    n

---
+++ faa.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+PRUNE_POSTFIX="$HOME/bashrc/prunefix"
+PRUNE_FILE="$HOME/bashrc/prunefile"
+prune_params=();
+prune_files=();
+find_result="`echo "$2".faa.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
+if [ -f "$find_result" ]; then
+  read -p "the search is already done, if you want to update, press u --> " update
+  case $update in
+    u)
+      ;;
+    ?)
+      vi "$find_result"
+      exit 1
+      ;;
+  esac
+fi
+or="";
+grep_params="";
+if [ -n "$3" ]
+then grep_params=" -A"$3" -B"$3;
+fi
+or="";
+while read suf
+do
+  prune_params+=( $or "-iname" "*.$suf" )
+  or="-o"
+done < "$PRUNE_POSTFIX"
+while read suf
+do
+  prune_files+=( $or "-iname" "$suf" )
+  or="-o"
+done < "$PRUNE_FILE"
+find -L "$1" "(" "${prune_params[@]}" "${prune_files[@]}" "-o" "-name" "$find_result" ")" -prune -o -type f -exec fgrep -wnH  $grep_params "$2" {} \; > "$find_result"
+~/bashrc/cscope.sh
+vi "$find_result"
