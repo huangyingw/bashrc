faee4f2ea2f2e298efea6633158d3deb5a88331b
huangyingw@linux_programming <huangyingw@gmail.com>
2014-09-20 23:05:47 -0700

    n

--- wol.sh
+++ wol.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-  wakeonlan b8:70:f4:ce:3d:f3 #toshiba
-  ping toshiba
+  wakeonlan 00:01:c0:12:92:a2 #mldonkey
+  ping mldonkey
 elif [ "$1" == "thinkpad" ]; then
   wakeonlan 00:21:cc:c0:53:a9 #thinkpad
   ping thinkpad
-elif [ "$1" == "mldonkey" ]; then
-  wakeonlan 00:01:c0:12:92:a2 #mldonkey
-  ping mldonkey
+elif [ "$1" == "toshiba" ]; then
+  wakeonlan b8:70:f4:ce:3d:f3 #toshiba
+  ping toshiba
 elif [ "$1" == "mini" ]; then
   wakeonlan 00:01:c0:12:94:05 #mini
   ping mini
