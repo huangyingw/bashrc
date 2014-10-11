c4c2a4d2647d330e0828cf39796f07963a084e9b
huangyingw@linux_programming <huangyingw@gmail.com>
2014-10-11 10:21:08 -0700

    n

--- mklnk.sh
+++ mklnk.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
 SOURCE=$1
 TARGET=$2
+
+if [ -L ${1%/} ] || [ -L ${2%/} ]
+then
+  echo -e "${red}please not use simbo link ! ... ${NC}"
+  exit 1
+fi
+
 LNSOURCE=`echo $SOURCE | sed 's/\/$//'`
 
 ~/bashrc/move.sh "$SOURCE" "$TARGET" \
