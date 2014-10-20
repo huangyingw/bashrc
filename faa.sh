8bc2fbd1b01cb6c1c26e6f54ae0053831c5ae2ec
huangyingw@linux_programming <huangyingw@gmail.com>
2014-10-20 11:07:41 -0700

    n

--- faa.sh
+++ faa.sh
@@ -32,4 +32,8 @@ do
   or="-o"
 done < "$PRUNE_FILE"
 find -L "$1" "(" "${prune_params[@]}" "${prune_files[@]}" "-o" "-name" "$find_result" ")" -prune -o -type f -exec fgrep -wnH  $grep_params "$2" {} \; > "$find_result"
-~/bashrc/cscope.sh
+if [ -f cscope.out ]; then
+  ~/bashrc/cscope.sh
+else
+  vi "$find_result"
+fi
