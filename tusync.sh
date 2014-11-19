998517d58649692dbf6e2e28cfd3c47a3dc25a2f
huangyingw@linux_programming <huangyingw@gmail.com>
2014-11-18 21:43:38 -0800

    n

---
+++ tusync.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+TARGET=exp
+if [ -n "$1" ]
+then 
+  TARGET="$1"
+fi
+echo $TARGET
+UBAKEX="$HOME/bashrc/usync_ex"
+UBAKIN="$HOME/bashrc/usync_in"
+exclude_params=();
+while read suf
+do
+    exclude_params+=( "--exclude=$suf" )
+done < "$UBAKEX"
+include_params=();
+while read suf
+do
+    include_params+=( "--include=$suf" )
+done < "$UBAKIN"
+rsync -aHinv --force -e ssh "${exclude_params[@]}" "${include_params[@]}" /home/huangyingw/ "${TARGET}":/home/huangyingw/> find.findresult 
