635a5d365c10621d6099e5154cff1df494842dfb
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-06 11:23:40 -0800

    n

--- vaa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-upsearch () {
-  test / == "$PWD" && return || test -e "$1" && echo "found: " "$PWD" && return || cd .. && upsearch "$1"
-}
-upsearch "cscope.out"
-~/bashrc/faa.sh `pwd` "$2"
