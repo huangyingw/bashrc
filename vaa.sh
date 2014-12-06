a9ecf28aa938a489be06b6400e12414c80b0c81c
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-05 17:56:40 -0800

    n

---
+++ vaa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+keyWord="$2"
+function upsearch () {
+  test / == "$PWD" && return || test -e "$1" && echo "found: " "$PWD" && return || cd .. && upsearch "$1"
+}
+upsearch "cscope.out"
+parent=
+~/bashrc/faa.sh `pwd` "$2"
