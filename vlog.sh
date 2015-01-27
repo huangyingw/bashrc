af99693bc08da1835fd9cd64a584ac2808e00189
huangyingw@linux_programming <huangyingw@gmail.com>
2015-01-27 13:40:14 -0800

    n

--- vlog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET='/export/home1/username/cscope_db/cscope.files'
-FILE_POSTFIX=$HOME/bashrc/postfix
-find `pwd`/ -type f \
-  -name \*log\* \
-  |sed 's/\(["'\''\]\)/\\\1/g;s/^/"/;s/$/"/' \
-  > ${TARGET}
-cscope -kqR -i ${TARGET} 
