76156251461be811ed9fc24bed73bafad3b9fe20
huangyingw@linux_programming <huangyingw@gmail.com>
2015-03-27 19:52:50 -0700

    n

--- move.sh
+++ move.sh
@@ -3,18 +3,17 @@ red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
 
+if [ ! -d "$2" ];
+then   
+  mkdir -p "$2"
+fi
+
 SOURCE=`realpath "$1"`
 TARGET=`realpath "$2"`
 
 if [ "$SOURCE" != "$TARGET" ]
 then
-  if [ ! -d "$TARGET" ];
-  then   
-    mkdir -p "$TARGET"
-  fi
-  ~/bashrc/copy.sh "$1" "$2" \
-  && ~/bashrc/copy.sh "$1" "$2" \
-  && rsync --remove-source-files -aH --force "$1" "$2" \
+  rsync --remove-source-files -aH --force "$1" "$2" \
     && rm -fr "$1"
 else
   echo -e "${red}same dir --> ${SOURCE} ... ${NC}"
